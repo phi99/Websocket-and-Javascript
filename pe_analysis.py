3c3f4eb8381bdd0e43d0afd09b30425b4151f502
phi99 <34248917+phi99@users.noreply.github.com>
2019-07-08 05:20:12 +1000

    Update pe_analysis.py

--- pe_analysis.py
+++ pe_analysis.py
@@ -1,10 +1,10 @@
 import pefile 
-import argparse 
 import sys 
  
 input_file=sys.argv[1]
 pe=pefile.PE(input_file) 
- 
+
+#List Sections of the file
 for section in pe.sections: 
     print("Sections:{}, Virtual Address:{}\n".format(section.Name,hex(section.VirtualAddress)))   
  
@@ -12,6 +12,7 @@ print("-------------------------------------------")
 print("               IAT LIST") 
 print("-------------------------------------------") 
  
+#List Import Address Table of the file
 for entry in pe.DIRECTORY_ENTRY_IMPORT: 
     print entry.dll 
     for function in entry.imports: 
