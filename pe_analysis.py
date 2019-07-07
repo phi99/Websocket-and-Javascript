999d6732c1491729d1a0ad787af89a6fc5139119
phi99 <34248917+phi99@users.noreply.github.com>
2019-07-08 04:57:48 +1000

    Update pe_analysis.py

--- pe_analysis.py
+++ pe_analysis.py
@@ -6,10 +6,12 @@ print(input_file)
 type(input_file)
 pe=pefile.PE(input_file)
 
+#Enumerate the file's PE Sections
 for section in pe.sections:
   print (section.Name, hex(section.VirtualAddress),
     hex(section.Misc_VirtualSize), section.SizeOfRawData )
 
+#Enumerate the file's Import Address Table
 for entry in pe.DIRECTORY_ENTRY_IMPORT:
     print entry.dll
     for function in entry.imports:
